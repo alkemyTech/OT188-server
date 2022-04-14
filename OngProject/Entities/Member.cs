951931d4c714d9098d9d981700c72c8915b81130
claudiodamico <cdamico18@gmail.com>
2022-04-14 11:48:46 -0300

    Add model members

---
+++ OngProject/Entities/Member.cs
@@ -0,0 +1,25 @@
+﻿using OngProject.Repositories.Interfaces;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Member 
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public int Name { get; set; }
+        [MaxLength(255)]
+        public string FacebookUrl { get; set; }
+        [MaxLength(255)]
+        public string InstagramUrl { get; set; }
+        [MaxLength(255)]
+        public string LinkedinUrl { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
