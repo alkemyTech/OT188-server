b2d8e6866b13bf54efdaa5fa8a34a92bb63714af
JVita-Code <juanvitan@gmail.com>
2022-04-17 17:05:43 -0300

    created Controller and Service for Members

---
+++ OngProject/Core/Business/MembersService.cs
@@ -0,0 +1,18 @@
+﻿using OngProject.Core.Interfaces;
+using OngProject.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Business
+{
+    public class MembersService : IMembersService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MembersService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
