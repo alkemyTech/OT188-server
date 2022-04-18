b5356b9dba24356db078a0c112572aef86ebc616
Nicolas Tomeo <87679478+nickt64@users.noreply.github.com>
2022-04-18 16:48:59 -0300

    Change to Entities/Contact

---
+++ OngProject/Entities/Contact.cs
@@ -0,0 +1,26 @@
+﻿using OngProject.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Contact:EntityBase
+    {
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        [Phone]
+        [MaxLength(20)]
+        public int Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [MaxLength(320)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Messege { get; set; }
+    }
+}
