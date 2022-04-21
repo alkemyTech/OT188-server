a8cb046e5c2b9b92c13464a4181306e58495b146
Mariano Incinga <mariano0_g@hotmail.com>
2022-04-21 18:32:36 -0300

    Added Organization DTO class.

---
+++ OngProject/Core/Models/DTOs/OrganizationDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace OngProject.Core.Models.DTOs
+{
+    public class OrganizationDTO
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+    }
+}
