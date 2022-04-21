f4a66981a99fd114e1c179e4d94128683716d98e
Mariano Incinga <mariano0_g@hotmail.com>
2022-04-21 18:40:55 -0300

    Added DTO Converter to IEntityMapper.

--- OngProject/Core/Interfaces/IEntityMapper.cs
+++ OngProject/Core/Interfaces/IEntityMapper.cs
@@ -1,10 +1,11 @@
 ﻿using OngProject.Core.Models.DTOs;
 using OngProject.Entities;
+using System.Threading.Tasks;
 
 namespace OngProject.Core.Interfaces
 {
     public interface IEntityMapper
     {
-        
+        public Task<OrganizationDTO> ConvertToOrganizationDTO(Organization organization);
     }
 }
