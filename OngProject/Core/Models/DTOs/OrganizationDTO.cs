8c2c86aef6746aea7224272b72de76c7a121532e
Mariano Incinga <mariano0_g@hotmail.com>
2022-04-22 01:46:04 -0300

    DTO Fixes

--- OngProject/Core/Models/DTOs/OrganizationDTO.cs
+++ OngProject/Core/Models/DTOs/OrganizationDTO.cs
@@ -1,9 +1,16 @@
-﻿namespace OngProject.Core.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
 {
     public class OrganizationDTO
     {
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
         public string ImageUrl { get; set; }
+        [StringLength(20)]
         public string? Phone { get; set; }
         public string? Address { get; set; }
     }
