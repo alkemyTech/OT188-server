f3c0b1156cb7b059871e6654b3e298ab1899198e
JVita-Code <juanvitan@gmail.com>
2022-04-21 23:46:19 -0300

    created ContactsController with empty methods.

---
+++ OngProject/Controllers/ContactsController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("api/contacts")]
+    [ApiController]
+    [Authorize(Roles = "Administrator")]
+
+    public class ContactsController : ControllerBase
+    {
+        public ContactsController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        [Route("{id}")]
+        [HttpGet]
+        public IActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Insert()
+        {
+            return Ok();
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            return NoContent();
+        }
+
+        [Route("{id}")]
+        [HttpPatch]
+        public async Task<IActionResult> Update()
+        {
+            return NoContent();
+        }
+    }
+}
