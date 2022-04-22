b329e43c054e3458762a6d59c3f49b50eca1df9f
Nicolas Tomeo <87679478+nickt64@users.noreply.github.com>
2022-04-22 14:42:21 -0300

    Create SeedCategories  Add modelBuilder

---
+++ OngProject/DataAccess/SeedsData/SeedCategories.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OngProject.Entities;
+
+namespace OngProject.DataAccess.SeedsData
+{
+    public class SeedCategories : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
