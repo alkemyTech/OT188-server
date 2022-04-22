c745dbebb4f356fb2a4de6bf6e0df7e22127ad91
Nicolas Tomeo <87679478+nickt64@users.noreply.github.com>
2022-04-22 15:50:03 -0300

    Add Data

--- OngProject/DataAccess/SeedsData/SeedCategories.cs
+++ OngProject/DataAccess/SeedsData/SeedCategories.cs
@@ -6,9 +6,21 @@ namespace OngProject.DataAccess.SeedsData
 {
     public class SeedCategories : IEntityTypeConfiguration<Category>
     {
-        public void Configure(EntityTypeBuilder<Category> builder)
-        {
-            throw new System.NotImplementedException();
-        }
+		public void Configure(EntityTypeBuilder<Category> builder)
+		{
+			for (int i = 1; i < 11; i++)
+			{
+				builder.HasData(
+					new Category
+					{
+						Id = i,
+						Name = "Categoria " + i,
+						Description = "descripcion de categoria " + i,
+						Image = "Imágenes de categoria " + i,
+
+					}
+				);
+			}
+		}
     }
 }
