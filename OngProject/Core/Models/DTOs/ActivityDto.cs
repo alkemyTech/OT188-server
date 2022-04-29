1779b638acc670b17d97e4cf091dd8c4028e3536
Mariano Incinga <mariano0_g@hotmail.com>
2022-04-29 00:07:15 -0300

    Added properties ActivityDto.

--- OngProject/Core/Models/DTOs/ActivityDto.cs
+++ OngProject/Core/Models/DTOs/ActivityDto.cs
@@ -1,6 +1,17 @@
-﻿namespace OngProject.Core.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
 {
     public class ActivityDto
     {
+        [Required(ErrorMessage = "\"Name\" field is required")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "\"Content\" field is required")]
+        [MaxLength(65535)]
+        public string Content { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Image { get; set; }
     }
 }
