384802942d94ddf64755289e86f9fd9afbae666a
BrianFranco <58949010+BrianFranco@users.noreply.github.com>
2022-04-29 16:38:18 -0300

    Create Helper AmazonS3Helper and AWSConfiguration

--- OngProject/Core/Interfaces/IAmazonS3Helper.cs
+++ OngProject/Core/Interfaces/IAmazonS3Helper.cs
@@ -1,7 +1,13 @@
-﻿namespace OngProject.Core.Interfaces
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Interfaces
 {
     public interface IAmazonS3Helper
     {
         //métodos.
+        Task<List<string>> GetAllFiles();
+        Task<string> UploadFileAsync(IFormFile file);
     }
 }
