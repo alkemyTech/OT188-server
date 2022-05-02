5b501858e5b6867d85a127f7f3f555822fc64dc2
Mariano Incinga <mariano0_g@hotmail.com>
2022-05-02 18:43:30 -0300

    Added newTestimonyDto

---
+++ OngProject/Core/Models/DTOs/NewTestimonyDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class NewTestimonyDto
+    {
+        [Required(ErrorMessage = "Name field is required")]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Image { get; set; }
+
+        [Required(ErrorMessage = "Description field is required")]
+        [MaxLength(255)]
+        public string Description { get; set; }
+    }
+}
