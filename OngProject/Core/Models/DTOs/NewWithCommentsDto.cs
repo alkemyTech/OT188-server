ed25546d393258e7feffdb5fae9d7a7c4cceb76f
JVita-Code <juanvitan@gmail.com>
2022-05-03 21:35:37 -0300

    created Dto

---
+++ OngProject/Core/Models/DTOs/NewWithCommentsDto.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class NewWithCommentsDto
+    {
+        public List<CommentOutDto> Comments { get; set; }
+    }
+}
