f562b64db242378c1ffa6640afe7aacc6b4a861b
Fernando M. Bruzzo <fernandobruzzo7@gmail.com>
2022-05-04 23:43:59 -0300

    fix endpoint activity

---
+++ OngProject/Core/Models/DTOs/ActivityOutDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OngProject.Core.Models.DTOs
+{
+    public class ActivityOutDTO
+    {
+        public string Name { get; set; }
+
+        public string Content { get; set; }
+        
+        public string Image { get; set; }
+    }
+}
