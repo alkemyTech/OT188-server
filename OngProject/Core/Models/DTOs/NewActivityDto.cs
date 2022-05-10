ac0ae306c8cd15a8fb1af9f6cb35907a63a41da9
Nicolas Tomeo <87679478+nickt64@users.noreply.github.com>
2022-05-10 16:40:22 -0300

    documentation activity

--- OngProject/Core/Models/DTOs/NewActivityDto.cs
+++ OngProject/Core/Models/DTOs/NewActivityDto.cs
@@ -5,12 +5,25 @@ namespace OngProject.Core.Models.DTOs
 {
     public class NewActivityDto
     {
+        /// <summary>
+        /// activity name.
+        /// </summary>
+        /// <example>webinar</example>
         [Required(ErrorMessage = "Name field is required")]
         [MaxLength(255)]
         public string Name { get; set; }
+        /// <summary>
+        /// content activity
+        /// </summary>
+        /// 
         [Required(ErrorMessage = "Content field is required")]
         [MaxLength(65535)]
+
         public string Content { get; set; }
+        /// <summary>
+        /// image file
+        /// </summary>
+        
         public IFormFile Image { get; set; }
     }
 }
--- OngProject/Core/Models/DTOs/UpdateActivityDTO.cs
+++ OngProject/Core/Models/DTOs/UpdateActivityDTO.cs
@@ -5,10 +5,23 @@ namespace OngProject.Core.Models.DTOs
 {
     public class UpdateActivityDTO
     {
+        /// <summary>
+        /// activity name.
+        /// </summary>
+        /// <example>webinar xxx</example>
         [MaxLength(255)]
         public string? Name { get; set; }
+        /// <summary>
+        /// activity content.
+        /// </summary>
+       
         [MaxLength(65535)]
         public string? Content { get; set; }
+
+        /// <summary>
+        /// image file
+        /// </summary>
+        
         public IFormFile? Image { get; set; }
     }
 }
