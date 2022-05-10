ca15fbb681a21ba130a4708db56785f62140b6fc
JVita-Code <juanvitan@gmail.com>
2022-05-10 08:58:52 -0300

    added Dtos to update New

---
+++ OngProject/Core/Models/DTOs/UpdateNewDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class UpdateNewDto
+    {
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public IFormFile Image { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
