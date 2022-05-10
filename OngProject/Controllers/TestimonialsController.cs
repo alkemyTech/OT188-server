963b111af3d850e9c0563adeb70846a934dcb2df
SDLucasFerrenti <lukasferrenti@hotmail.com>
2022-05-10 09:18:27 -0300

    document-testimonials

--- OngProject/Controllers/TestimonialsController.cs
+++ OngProject/Controllers/TestimonialsController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.Interfaces;
 using OngProject.Core.Models;
@@ -11,6 +12,7 @@ using OngProject.Entities;
 
 namespace OngProject.Controllers
 {
+
     [Route("testimonials")]
     [ApiController]
     public class TestimonialsController : ControllerBase
@@ -21,8 +23,15 @@ namespace OngProject.Controllers
         {
             _business = business;
         }
-       
+        /// <summary>
+        /// Create new Testimony
+        /// </summary>
+        /// <remarks>To create a new testimonial indicate name and description, and optional the image</remarks>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Authorize(Roles = "Administrator")]
         public async Task<ActionResult> Post([FromForm] NewTestimonyDto newEntity)
         {
@@ -43,8 +52,15 @@ namespace OngProject.Controllers
                 return StatusCode(500, new Response<NewTestimonyDto>(data: null, succeeded: false, errors: listErrors, message: "Server Error"));
             }
         }
-
+        /// <summary>
+        /// Delete specific testimony by id
+        /// </summary>
+        /// <param name="id">Numeric identifier from testimony</param>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         [Authorize(Roles = "Administrator")]
         public async Task<IActionResult> Delete(int id)
         {
