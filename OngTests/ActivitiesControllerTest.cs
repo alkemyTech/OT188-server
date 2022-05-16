148dcaaafdf0a77017c47c10b42036d120c341e8
Mariano Incinga <mariano0_g@hotmail.com>
2022-05-16 20:36:09 -0300

    Add ActivitiesControllerTest class

---
+++ OngTests/ActivitiesControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using OngProject.Controllers;
+using OngProject.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OngTests
+{
+    [TestClass]
+    internal class ActivitiesControllerTest
+    {
+        private readonly ActivitiesController _activitiesController;
+        private readonly Mock<IActivitiesBusiness> _activitiesBusiness;
+        private readonly Mock<IAmazonS3Helper> _amazonS3;
+
+        public ActivitiesControllerTest(ActivitiesController activitiesController)
+        {
+            _activitiesController = activitiesController;
+        }
+
+        [TestMethod()]
+        private async Task Insert_Should()
+        {
+
+        }
+    }
+}
