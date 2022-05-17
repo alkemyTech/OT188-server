60ae59071a751b16bbc1408b0d24b1d2c88ac7e4
Mariano Incinga <mariano0_g@hotmail.com>
2022-05-16 23:39:07 -0300

    Add tests for insert activity method

--- OngTests/ActivitiesControllerTest.cs
+++ OngTests/ActivitiesControllerTest.cs
@@ -1,31 +1,97 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using OngProject.Controllers;
 using OngProject.Core.Interfaces;
+using OngProject.Core.Models;
+using OngProject.Core.Models.DTOs;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace OngTests
 {
     [TestClass]
-    internal class ActivitiesControllerTest
+    public class ActivitiesControllerTest
     {
         private readonly ActivitiesController _activitiesController;
-        private readonly Mock<IActivitiesBusiness> _activitiesBusiness;
-        private readonly Mock<IAmazonS3Helper> _amazonS3;
+        private readonly Mock<IActivitiesBusiness> _activitiesBusiness= new Mock<IActivitiesBusiness>();
+        private readonly Mock<IAmazonS3Helper> _amazonS3 = new Mock<IAmazonS3Helper>();
 
-        public ActivitiesControllerTest(ActivitiesController activitiesController)
+        public ActivitiesControllerTest()
         {
-            _activitiesController = activitiesController;
+            _activitiesController = new ActivitiesController(_activitiesBusiness.Object);
         }
 
-        [TestMethod()]
-        private async Task Insert_Should()
+        [TestMethod]
+        public async Task Insert_Should_Return_200_And_ActivityOutDto()
         {
+            //Arrange
+            var file = new Mock<IFormFile>();
+            var activityDto = new NewActivityDto()
+            {
+                Name = "Dancing",
+                Content = "Dancing Activity",
+                Image = file.Object
 
+            };
+            var activityOutDto = new ActivityOutDTO()
+            {
+                Name = "Dancing",
+                Content = "Dancing Activity",
+                Image = await _amazonS3.Object.UploadFileAsync(file.Object)
+            };
+
+            _amazonS3.Setup(x => x.UploadFileAsync(It.IsAny<IFormFile>())).ReturnsAsync("photoUrl").Verifiable();
+            _activitiesBusiness.Setup(x => x.InsertActivity(It.IsAny<NewActivityDto>())).ReturnsAsync(new Response<ActivityOutDTO>(activityOutDto)).Verifiable();
+
+            //Act
+            var response = await _activitiesController.Insert(activityDto);
+            var result = response as ObjectResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(200, result.StatusCode);
+            Assert.IsInstanceOfType(result.Value, typeof(Response<ActivityOutDTO>));
+
+            _amazonS3.Verify(x => x.UploadFileAsync(It.IsAny<IFormFile>()), Times.Once);
+            _activitiesBusiness.Verify(x => x.InsertActivity(It.IsAny<NewActivityDto>()), Times.Once);
         }
+
+        [TestMethod]
+        public async Task Insert_Should_Return_500_If_Server_Fails()
+        {
+            //Arrange
+            var file = new Mock<IFormFile>();
+            var activityDto = new NewActivityDto()
+            {
+                Name = "Dancing",
+                Content = "Dancing Activity",
+                Image = file.Object
+
+            };
+            var activityOutDto = new ActivityOutDTO()
+            {
+                Name = "Dancing",
+                Content = "Dancing Activity",
+                Image = await _amazonS3.Object.UploadFileAsync(file.Object)
+            };
+
+            _amazonS3.Setup(x => x.UploadFileAsync(It.IsAny<IFormFile>())).ReturnsAsync("photoUrl").Verifiable();
+            _activitiesBusiness.Setup(x => x.InsertActivity(It.IsAny<NewActivityDto>())).Throws(new Exception()).Verifiable();
+
+            //Act
+            var response = await _activitiesController.Insert(activityDto);
+            var result = response as ObjectResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(500, result.StatusCode);
+            Assert.IsInstanceOfType(result.Value, typeof(Response<NewActivityDto>));
+
+            _amazonS3.Verify(x => x.UploadFileAsync(It.IsAny<IFormFile>()), Times.Once);
+            _activitiesBusiness.Verify(x => x.InsertActivity(It.IsAny<NewActivityDto>()), Times.Once);
+        }
+
     }
 }
